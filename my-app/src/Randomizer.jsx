f289dbb94291ce04c3254e0dd09a3b39946c8720
Lovisa Landberg <lovisa@landberg.org>
2025-06-17 14:19:00 +0200

    Kommenterade Randomizer

--- my-app/src/Randomizer.jsx
+++ my-app/src/Randomizer.jsx
@@ -1,39 +1,41 @@
 import React, { useEffect, useState } from 'react';
 
 const promotionalItems = [
-  {
-    id: 1,
-    image: './img/kyckling-med-cashew.png',
-    description: 'Rea på kyckling med cashewnötter!!!',
-  },
-  {
-    id: 2,
-    image: './img/lökringar.jpg',
-    description: 'Superdeal på lökringar :)',
-  },
-  {
-    id: 3,
-    image: './img/räkor.jpg.webp',
-    description: 'Rea rea rea på räkor i stark vitlökssås!!!',
-  },
+	{
+		id: 1,
+		image: './img/kyckling-med-cashew.png',
+		description: 'Rea på kyckling med cashewnötter!!!',
+	},
+	{
+		id: 2,
+		image: './img/lökringar.jpg',
+		description: 'Superdeal på lökringar :)',
+	},
+	{
+		id: 3,
+		image: './img/räkor.jpg.webp',
+		description: 'Rea rea rea på räkor i stark vitlökssås!!!',
+	},
 ];
 
 function Randomizer() {
-  const [selectedItem, setSelectedItem] = useState(null);
+	const [selectedItem, setSelectedItem] = useState(null);
 
-  useEffect(() => {
-    const randomIndex = Math.floor(Math.random() * promotionalItems.length);
-    setSelectedItem(promotionalItems[randomIndex]);
-  }, []);
+	/* Det som faktiskt uppdaterar värdet */
+	useEffect(() => {
+		const randomIndex = Math.floor(Math.random() * promotionalItems.length);
+		setSelectedItem(promotionalItems[randomIndex]);
+	}, []);
 
-  if (!selectedItem) return null;
+	/*Om inget har valts visas inget*/
+	if (!selectedItem) return null;
 
-  return (
-    <div className="container">
-      <img src={selectedItem.image} alt={`Product ${selectedItem.id}`} className='promo-image'/>
-      <p>{selectedItem.description}</p>
-    </div>
-  );
+	return (
+		<div className="container">
+			<img src={selectedItem.image} alt={`Product ${selectedItem.id}`} className='promo-image' />
+			<p>{selectedItem.description}</p>
+		</div>
+	);
 }
 
 export default Randomizer;
